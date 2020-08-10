84be7613ac2201b3fd491e11313fb3f580541191
Juanjo Gamboa <juanjogam@gmail.com>
2020-08-10 12:39:23 +0200

    Update setup_root.sh

--- setup_root.sh
+++ setup_root.sh
@@ -3,17 +3,19 @@
 pacman-key --init
 pacman-key --populate archlinuxarm
 pacman -Syu
-pacman -S sudo uboot-tools
+pacman -S sudo git # uboot-tools (only in AARCH64)
 hostnamectl set-hostname Neverwhere
 usermod -d /home/trekirk -m alarm
 usermod -l trekirk alarm
 groupmod -n trekirk alarm
+usermod -aG wheel,audio,video,optical,storage trekirk
 # nano /boot/boot.txt 
 # cat /proc/cmdline 
 # ./mkscr 
 # visudo
 timedatectl set-ntp true
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+visudo
 # vim /etc/locale.gen 
 # locale-gen
 # vim /etc/locale.conf 
