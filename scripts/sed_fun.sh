9910c44d140c4d30914efaf302765d63104f6f4b
Juanjo Gamboa <juanjogam@gmail.com>
2020-08-14 18:41:11 +0200

    Add sed script for ChucK

---
+++ scripts/sed_fun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -r 's/.*audio device: ([0-9]+).*/Card #\1/g' > chuck_cards.txt
+chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -r 's/\[chuck\]: /\n/g' > chuck_pretty.txt
