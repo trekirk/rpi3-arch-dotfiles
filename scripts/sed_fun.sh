8f975c2e82c04425d7a65f1944113b691a6059dd
Juan José Gamboa <15802647+trekirk@users.noreply.github.com>
2020-08-20 17:12:47 +0200

    Update sed_fun.sh

--- scripts/sed_fun.sh
+++ scripts/sed_fun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -r 's/.*audio device: ([0-9]+).*/Card #\1/g' > chuck_cards.txt
-chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -r 's/\[chuck\]: /\n/g' > chuck_pretty.txt
-chuck --version 2>&1 > /dev/null | sed -rn '/linux \(/p' | sed -E 's/.+linux \((.+)\).+/\1/g'
+chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -E 's/.*audio device: ([0-9]+).*/Card #\1/g' > chuck_cards.txt
+chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -E 's/\[chuck\]: /\n/g' > chuck_pretty.txt
+chuck --version 2>&1 > /dev/null | sed -En '/linux \(/p' | sed -E 's/.+linux \((.+)\).+/\1/g'
