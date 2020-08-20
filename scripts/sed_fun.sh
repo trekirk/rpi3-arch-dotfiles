86eb46038208039d0587f7ee10950be02ecc50ae
Juan José Gamboa <15802647+trekirk@users.noreply.github.com>
2020-08-20 19:09:49 +0200

    Update sed_fun.sh with more robust sed commands

--- scripts/sed_fun.sh
+++ scripts/sed_fun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -E 's/.*audio device: ([0-9]+).*/Card #\1/g' > chuck_cards.txt
-chuck --probe 2>&1 > /dev/null | sed -n '/Sound Blaster Play/p' | sed -E 's/\[chuck\]: /\n/g' > chuck_pretty.txt
+chuck --probe 2>&1 > /dev/null | tr '\n' '\r' | sed -E 's/\r(\[chuck\]\: ------\( audio)/\n\1/g' | sed -n '/Sound Blaster Play/p' | sed -E 's/.*audio device: ([0-9]+).*/Card #\1/g' > chuck_cards.txt
+chuck --probe 2>&1 > /dev/null | tr '\n' '\r' | sed -E 's/\r(\[chuck\]\: ------\( audio)/\n\1/g' | sed -n '/Sound Blaster Play/p' | sed -E 's/\[chuck\]: /\n/g' > chuck_pretty.txt
 chuck --version 2>&1 > /dev/null | sed -En '/linux \(/p' | sed -E 's/.+linux \((.+)\).+/\1/g'
